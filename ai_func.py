f228231e2a1ac2682886cfd5c71f850bd06a2e40
AviadDerli <aviad@workin.co.il>
2025-01-07 20:53:52 +0200

    sadasd

--- ai_func.py
+++ ai_func.py
@@ -1,8 +1,10 @@
 def ai_calc(a,b):
-    print("bla bla")
+    print("🍕🍔🍟🌭")
 def ai_calc2(a,b):
     print("bla bla")
-def ai_calc3(a,b):
+def ai_calc3(e,q):
     print("bla bla")
 def ai_calc4(a,b):
+    print("bla bla")
+def ai_calc5(a,b):
     print("bla bla")

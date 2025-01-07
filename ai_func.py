8ecf9997dfa4e16e715746ea2c82b201070857ac
AviadDerli <aviad@workin.co.il>
2025-01-07 20:52:14 +0200

    bla bla bla

--- ai_func.py
+++ ai_func.py
@@ -1,8 +1,12 @@
-def ai_calc(a,b):
-    print("bla bla")
+def ai_calc(a,t):
+    b = a
+    t = b
+    a = t
+    print(a,t)
 def ai_calc2(a,b):
     print("bla bla")
-def ai_calc3(a,b):
-    print("bla bla")
+def ai_calc3(c,d):
+    num=10
+    print(num*22)
 def ai_calc4(a,b):
     print("bla bla")
